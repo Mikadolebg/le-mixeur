25568cb29aaa6841ce4d3198bb8ab7e516f02d03
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-03-13 23:40:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
    console.log(`(1) Logged in as ${client.user.tag}!`);
-   client.user.setGame('vec la mort.');
+   client.user.setGame('la mort.');
    client.setMaxListeners(1);
   });
 // création d'une commande (message, reponse)
