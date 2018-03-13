3f8e6845d2004534aae5da446e307d28b56dace6
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-03-13 16:52:27 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+// on off
+// var
+const restify = require("restify");
+var server = restify.createServer();
+server.listen(process.env.PORT || 5000, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
+// Ne change pas le script ce POINT
+
+
+// language utiliser par le bot 
+
+const Discord = require("discord.js");
+
+//Etat du bot Mikado BOT
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+   console.log(`(1) Logged in as ${client.user.tag}!`);
+   client.user.setGame('manger des Patates');
+   client.setMaxListeners(1);
+  });
+// création d'une commande (message, reponse)
+
+client.on('message', msg => {
+  if (msg.content === 'Mikado') {
+    msg.channel.send('Moi même \n *(msg auto)* ');
+  }
+
+    
+  // Création d'une commande (message, reponse)
+  
+    
+    else if (msg.content === 'Asu') {
+      msg.channel.send('Manges des NinjaCookies :cookie: !');
+      client.setMaxListeners(1);
+      client.setMaxListeners(1);
+    }
+     
+});
+
+// client secret
+  client.login(process.env.TOKEN);
