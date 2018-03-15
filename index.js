7028e1187a5a9ab31e45daf66212e3d851c62e7b
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-03-15 10:41:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ client.on('message', msg => {
     }
     
     else if (msg.content === 'helpmik') {
-      msg.channel.send('---------------------- \n__**Bot:**__ \n1=Rythm \n2=Ayana \n3=Koya \n4=DabBot \n---------------------- \n__**Commandes Bot:**_\nl=leave \nj=join \ns=skip \n---------------------- \n__**Raccourcis Play:**__ \nrnm=piano cover of "For The Damaged - Coda"');
+      msg.channel.send('------------------------------------------------ \n__**Bot:**__ \n1=Rythm \n2=Ayana \n3=Koya \n4=DabBot \n------------------------------------------------ \n__**Commandes Bot:**__ \nl=leave \nj=join \ns=skip \n------------------------------------------------ \n__**Raccourcis Play:**__ \nrnm=piano cover of "For The Damaged - Coda" \n------------------------------------------------');
       client.setMaxListeners(1);
       client.setMaxListeners(1);
     }
