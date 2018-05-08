f773bf74dfc29bd3b2f156d2f8ff78f7f59449d2
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-08 11:07:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-// on off
-// var
-const restify = require("restify");
-var server = restify.createServer();
-server.listen(process.env.PORT || 5000, function () {
-    console.log('%s listening to %s', server.name, server.url); 
-});
-// Ne change pas le script ce POINT
-
-
-// language utiliser par le bot 
 
 const Discord = require("discord.js");
-
-//Etat du bot Mikado BOT
-
 const client = new Discord.Client();
+const Google = require('./commands/google');
+const moment = require('moment');
 
 client.on('ready', () => {
    console.log(`(1) Logged in as ${client.user.tag}!`);
@@ -23,10 +11,8 @@ client.on('ready', () => {
   });
 // création d'une commande (message, reponse)
 
-client.on('message', msg => {
-  if (msg.content === 'ping32456') {
-    msg.channel.send('Pong');
-  }   
+bot.on('message', function(message) { 	
+    Google.parse(message)	
 });
     
     client.on('guildMemberAdd', member => {
