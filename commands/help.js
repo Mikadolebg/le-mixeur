4fcdd3fd8a5894e2c87e8d53b4afcecbb57c272c
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-09 19:23:48 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const Command = require('./command')
+
+module.exports = class Help extends Command {
+
+	static match(message) {
+  
+  return message.content('!help')
+	}
+
+	static action(message) {
+		var embed = new Discord.RichEmbed()
+	        .setColor("#F39C12")
+		.setAuthor('Liste des commandes :')
+                .addField(':rotating_light: Administration', '`[en dev]`')
+                .addField(':video_game: Fun', ' `!google`, `!say`')
+                .addField(':newspaper: Info', ' `!stats[en dev]`, `!invite[en dev]`, `!discord[en dev]`, `!ping[en dev]`, `!profile[en dev]`')
+                .setTimestamp()
+		message.channel.send(embed)
+    
+	     }
+  }
