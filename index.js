931d70d64e2fcc67f96dc76f4fbeec9e41065422
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-09 20:43:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const Google = require('./commands/google');
 const Say = require('./commands/say');
-const Help = require('./commands/help');
+//const Help = require('./commands/help');
 const moment = require('moment');
 
 bot.on('ready', () => {
@@ -16,9 +16,21 @@ bot.on('ready', () => {
 bot.on('message', function(message) { 	
     Google.parse(message)	
     Say.parse(message)
-    Help.parse(message)
-
- 
+   // Help.parse(message)
+	
+if(message.content === '!say2') {
+ var embed = new Discord.RichEmbed()
+	        .setColor("#F39C12")
+		.setAuthor('Liste des commandes :')
+                .addField(':rotating_light: Administration', '`[en dev]`')
+                .addField(':video_game: Fun', ' `!google`, `!say`')
+                .addField(':newspaper: Info', ' `!stats[en dev]`, `!invite[en dev]`, `!discord[en dev]`, `!ping[en dev]`, `!profile[en dev]`')
+                .setTimestamp()
+		message.channel.send(embed)
+    
+	     }
+  }
+	
   else if(message.content.startsWith('!say2')) {
        if (message.author.id !== '423118623876448296')
        if (message.author.id !== '301913733536415755')
