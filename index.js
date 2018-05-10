a5bdeae0a99003bb890f36dd1f63220132e0c651
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-10 14:41:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if(message.content.startsWith('!help')) {
     
     bot.on('guildMemberAdd', member => {
     console.log('Membre ' + member.user.username + " a rejoind le serveur")
-    member.guild.channels.find("id", "438695803901706253").send("Bienvenue à toi, tu es actuellement demandeur d'asile et la population risque d'être hostile à ton égard. Il te suffit de t'entretenir avec Monsieur Pêche pour qu'il te donne tes papiers et ainsi devenir citoyen.");
+    member.guild.channels.find("id", "438695803901706253").send( "Bienvenue à toi" + member.user.toString() + ", tu es actuellement demandeur d'asile et la population risque d'être hostile à ton égard. Il te suffit de t'entretenir avec Monsieur Pêche pour qu'il te donne tes papiers et ainsi devenir citoyen.");
     var role = member.guild.roles.find('name', "Demandeur d'asile");
     member.addRole(role);
     
