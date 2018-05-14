4f87a74e928ea6ddd4f0790079e664c64b3c248b
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-14 17:12:12 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,9 +14,10 @@ module.exports = class Help extends Command {
 	        .setColor("#F39C12")
 		.setAuthor('Liste des commandes :')
 		.addField('Prefix :', + '`' + prefix + '`')
-                .addField(':rotating_light: Administration', '`[en dev]`')
-                .addField(':video_game: Fun', ' `!google`, `!say`')
-                .addField(':newspaper: Info', ' `!stats[en dev]`, `!invite[en dev]`, `!discord[en dev]`, `!ping[en dev]`, `!profile[en dev]`')
+		.addField(':rotating_light: Administration', ' `clear`, `[en dev]`')
+                .addField(':video_game: Fun', ' `say`, `[en dev]`')
+                .addField(':mag_right: Recherche', ' `google`, `ytb`, `deezer`')
+                .addField(':newspaper: Info', ' `stats[en dev]`, `invite[en dev]`, `discord[en dev]`, `ping[en dev]`, `profile[en dev]`')
                 .setTimestamp()
 		message.channel.send(embed)
     
