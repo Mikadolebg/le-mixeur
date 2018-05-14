1b2552de954b67620ff870d0e191ff367de7c4e3
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-14 17:58:40 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports = class Help extends Command {
 	        .setColor("#F39C12")
 		.setAuthor('Liste des commandes :')
 		.addField('Prefix :', '`' + prefix + '`')
-		.addField(':rotating_light: Administration', '`' + prefix + 'say' + '`' + ',')
+		.addField(':rotating_light: Administration', '`' + prefix + 'clear' + '`' + ',')
                 .addField(':video_game: Fun', '`' + prefix + 'say' + '`' + ',' + '`[en dev]`')
                 .addField(':mag_right: Recherche', '`' + prefix + 'google' + '`' + ',' + '`' + prefix + 'ytb' + '`' + ',' + '`' + prefix + 'deezer' + '`' + ',')
                 .addField(':newspaper: Info', '`' + prefix + 'stats[en dev]' + '`' + ',' + '`' + prefix + 'invite[en dev]' + '`' + ',' + '`' + prefix + 'discord[en dev]' + '`' + ',' + '`' + prefix + 'ping[en dev]' + '`' + ',' + '`' + prefix + 'profile[en dev]' + '`' + ',')
