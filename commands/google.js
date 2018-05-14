cc70e2b8cf02194b308633dd23e85274e4078f00
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-14 16:48:56 +0200

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -1,9 +1,10 @@
 const Command = require('./command')
+const prefix = '!!'
 
 module.exports = class Google extends Command {
 
 	static match(message) {
-		return message.content.startsWith('!google')
+		return message.content.startsWith(prefix + 'google')
 	}
 
 	static action(message) {
