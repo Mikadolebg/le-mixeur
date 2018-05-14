fec0ea0d9a91264d1359d7168f680ba231887641
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-14 23:27:46 +0200

    Update skribble.js

--- commands/skribble.js
+++ commands/skribble.js
@@ -4,8 +4,6 @@ const prefix = '!!'
 
 module.exports = class Scribble extends Command {
 
-	if (message.content === 'skribble') {
-		message.channel.send('__**Toujours chaud pour un p\'tit skribble !**__ \nhttps://skribbl.io/')
-    
+
 	     }
   }
