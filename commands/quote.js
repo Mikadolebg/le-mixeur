dba98e1b03f91e9efa299c19f2369e32302256c1
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-17 19:30:21 +0200

    Create quote.js

---
+++ commands/quote.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const Command = require('./command')
+const prefix = '!!'
+
+module.exports = class Quote extends Command {
+
+
+
+	static match(message) {
+		return message.content.startsWith(prefix + 'quote')
+	}
+
+	static action(message) {
+
+		let randnum = Math.floor(Math.random() * 3)
+
+		if (randnum == 0) {
+
+			var embed = new Discord.RichEmbed()
+			.setColor("#AA1923")
+			.setDescription('Citation 1)
+      .setFooter('Citation de X')
+			message.channel.sendEmbed(embed)
+
+		} else if(randnum == 1) {
+
+			var embed = new Discord.RichEmbed()
+			.setColor("#AA1923")
+			.setDescription('Citation 2')
+      .setFooter('Citation de X')
+			message.channel.sendEmbed(embed)
+
+		} else if(randnum == 2) {
+
+			var embed = new Discord.RichEmbed()
+			.setColor("#AA1923")
+			.setDescription('Citation 3')
+      .setFooter('Citation de X')
+			message.channel.sendEmbed(embed)
+
+		} 
+			
+
+	}
+
+
+
+}
