20a62e14d5c7db3bf3e177fd3cfefe837e9fae5e
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-23 20:08:54 +0200

    Update pp.js

--- commands/pp.js
+++ commands/pp.js
@@ -1,6 +1,13 @@
-bot.on('message', function(message) {
-if (message.content === prefix + 'pp') {
+const Command = require('./command')
+const prefix = '!!'
+
+module.exports = class Youtube extends Command {
+
+	static match(message) {
+		return (message.content === prefix + 'pp') {
 	
+       static action(message) {
+		  
 	   var embed = new Discord.RichEmbed()
 	   .setColor("#2980b9")
 	   .setDescription('Voici ta photo de profile:')
