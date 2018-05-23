dcbed93fdd0f10a7f2de7c0e9bb2bd59a62bef51
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-23 20:15:42 +0200

    Update skribble.js

--- commands/skribble.js
+++ commands/skribble.js
@@ -3,6 +3,14 @@ const Command = require('./command')
 const prefix = '!!'
 
 module.exports = class Scribbl extends Command {
+	
+	if (message.content === prefix + 'skribbl') {
+	
+	   var embed = new Discord.RichEmbed()
+	   .setColor("#2980b9")
+	   .setDescription('__**Toujours chaud pour un p\'tit skribbl !**__')
+	    message.channel.send(embed)
+	    message.channel.send('https://skribbl.io/')
 
 
 	     }
