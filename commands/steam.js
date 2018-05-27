cfbf5df0c4d9deeb4556f032c455de97db413042
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-27 06:40:31 +0200

    Create steam.js

---
+++ commands/steam.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const Command = require('./command')
+const prefix = '!!'
+
+module.exports = class Steam extends Command {
+
+	static match(message) {
+		return message.content.startsWith(prefix + 'steam')
+	}
+
+	static action(message) {
+		
+			let args = message.content.split(" ").slice(1);
+			message.channel.send("https://store.steampowered.com/search/?term=" + args.join("+"))
+	}
+}
